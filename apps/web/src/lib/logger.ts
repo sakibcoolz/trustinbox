4156bdbc008d11b12b98fe8c2d784db961668873
sakibcoolz <sakibcoolz@gmail.com>
2026-03-31 02:51:24 +0530

    feat: add logger utility for provider and web applications

---
+++ apps/web/src/lib/logger.ts
@@ -0,0 +1,9 @@
+const PREFIX = "[web-app]";
+
+export const logger = {
+  log: (...args: unknown[]) => console.log(PREFIX, ...args),
+  warn: (...args: unknown[]) => console.warn(PREFIX, ...args),
+  error: (...args: unknown[]) => console.error(PREFIX, ...args),
+  info: (...args: unknown[]) => console.info(PREFIX, ...args),
+  debug: (...args: unknown[]) => console.debug(PREFIX, ...args),
+};
